3eace2ae55f13420f2604d540126566fc68ce097
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2026-08-23 10:36:57 +0700

    Atur px

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({
         rounded-full
         border border-slate-200
         bg-gray-100
-        px-4 py-2
+        px-2 py-2
         transition-all duration-300
         focus-within:border-gray-300
         focus-within:bg-white
